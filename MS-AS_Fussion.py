91970aa70cb6fb05f93cc58c7f3cb8a07effd2c0
Victor Calderon <vacalder@ncsu.edu>
2020-05-05 16:40:54 -0400

    Groundmotion Downloader

---
+++ MS-AS_Fussion.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 25 10:54:51 2019
+
+@author: vacalder
+"""
+
+# PROGRAM TO GENERATE MAINSHOCK - AFTERSHOCK RECORD SEQUENCES
+#   Victor A Calderon
+#   PhD Student/ Research Assistant
+#   NC STATE UNIVERSITY 
+#   2019 (c)
+import math  as math
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import fnmatch
+import shutil
+
+
+# Mainshock and Aftershock Paths
+
+MS_path= r'C:\ConditionDependent_PBD\EarthquakeSelection\MainShock_Test'
+AS_path= r'C:\ConditionDependent_PBD\EarthquakeSelection\Aftershock_Test'
+Seq_path= r'C:\ConditionDependent_PBD\EarthquakeSelection\Sequences_Test'
+
+
+# Reading file names
+
+MSListing = os.listdir(MS_path)
+ASListing =os.listdir(AS_path)
+MainshockFiles = []
+AftershockFiles = []
+
+
+for i in MSListing:
+    MainshockFiles.append(i)
+    
+
+for j in ASListing:
+    AftershockFiles.append(j)
+
+
+for x in MainshockFiles:
+    for y in AftershockFiles:
+        
+
+        with open(MS_path+'\\'+x) as GM1:
+            head1  = [next(GM1) for x in range(4)]
+            Data1 = GM1.read()
+            GM1.close
+        #print(head)
+        
+        EQ_DataLine1 = head1[3]
+        dt_pos1      = EQ_DataLine1.find("DT=")
+        
+        # Read from i to i+5 in line 3
+        
+        dt1  = float(EQ_DataLine1[dt_pos1+5:dt_pos1+11])
+        a1   = Data1.split()
+        A1   = [float(i) for i in a1]
+        NPT1 = len(A1)
+        TF1  = dt1*NPT1
+        t1   =np.linspace(dt1,TF1,NPT1)
+        
+        #Reading Aftershock
+        
+        with open(AS_path+'\\'+y) as GM2:
+            head2  = [next(GM2) for x in range(4)]
+            Data2 = GM2.read()
+            GM2.close
+        #print(head)
+        
+        EQ_DataLine2 = head2[3]
+        dt_pos2     = EQ_DataLine2.find("DT=")
+        
+        # Read from i to i+5 in line 3
+        
+        dt2 = float(EQ_DataLine2[dt_pos1+5:dt_pos1+10])
+        a2  = Data2.split()
+        A2   = [float(i) for i in a2]
+        
+        NPT2   = len(A2)
+        NPT2new=NPT2*(dt2/dt1)
+        TF2    = dt2*NPT2
+        t2     = np.linspace(dt2+TF1,TF2+TF1,NPT2)
+        t2_new = np.linspace(dt1+TF1,TF2+TF1,int(NPT2new))
+        A2_new = np.interp(t2_new,t2,A2)
+        
+        
+#        plt.plot(t1,A1)
+#        plt.plot(t2,A2)
+#        plt.plot(t2_new,A2_new)
+        
+        MS_AS=A1
+        MS_AS.extend(A2)
+        
+        with open(Seq_path+'\\'+x+y+'.g3', 'w') as f:
+            for item in MS_AS:
+                f.write("%s\n" % item)
---
+++ main.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 29 14:52:27 2019
+
+@author: VACALDER
+"""
+
+# PROGRAM TO FILTER GROUNDMOTIONS 
+# Victor A Calderon
+# PhD Student / Research Assistant
+# NC STATE UNIVERSITY 
+# 2019 (c)
+
+# The program has the capability to filter data according to different parameters
+# depending on the different properties listed in the PEER NGA West 2 DB
+# the program then can either download the files from PEER Website or from
+# Kowalsky Group Folder, the program will be further developed to download only
+# the files that are not downloaded since PEER website has a limit of 
+# 200 downloads every 2 weeks and 400 downloads per month, if the required downloads
+# are larger than this limit it is sugested to download a maximum of 200
+# this script is specifically designed to download sequences of Mainshocks and
+# Aftershocks.
+
+
+import math  as math
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.axes as ax
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from bs4 import BeautifulSoup
+import os
+import fnmatch
+import shutil
+
+
+# EARTHQUAKE SELECTION PROGRAM
+# Data base is taken fron NGA West 2 database from  PEER
+
+PeerDB = pd.read_csv('MS-AS_PEER_Website.csv')
+# Converting column names to lowercase and spaces changed to lower hyphen _
+PeerDB.columns=PeerDB.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '')
+
+# Data is filtered according to the following criteria:
+# * Moment Magnitude M=>5
+# * PGA>0.04
+# * PGV>1 cm/s
+# * Vs30>100m/s & Vs30<1000m/s
+# * Lowest ussable frequency is less than 1Hz
+# * Rrup<60km
+
+magnitude_filter=(PeerDB.earthquake_magnitude>=5)
+pga_filter=(PeerDB.pga_g>=0.04)
+pgv_filter=(PeerDB.pgv_cm_sec>=1)
+vs30_filter=(PeerDB.vs30_m_s_selectedforanalysis.between(100,1000))
+freq_filter=(PeerDB.lowestusablefreq_h1_hz<=1)
+rrup_filter=(PeerDB.joyner_booredist_km <= 60)
+counts = PeerDB['station_name'].value_counts()
+repeat=(PeerDB['station_name'].isin(counts.index[counts > 1]))
+all_filters=magnitude_filter & pga_filter & pgv_filter & vs30_filter & freq_filter & rrup_filter & repeat
+
+
+Peer_Filtered=PeerDB[all_filters]
+
+# Plotting Data Points
+
+Peer_Filtered.plot.scatter(x='joyner_booredist_km',y='earthquake_magnitude',s=20,c='earthquake_magnitude',colormap='viridis')
+plt.title('Main Shock and Aftershock Selection',fontsize=32)
+plt.xlabel('Rrup (km)',fontsize=24)
+plt.ylabel('Magnitude (Mw)',fontsize=24)
+plt.tick_params(direction='out',axis='both',labelsize=20)
+plt.show
+
+# Selecting Mainshocks and generate string to download RSN
+
+dwnstr=''
+
+for x in range(0,2):
+    print('the valu of x is ',x)
+    Main_Shocks=Peer_Filtered[Peer_Filtered['earthquake_name'].str.contains('0'+str(x))]
+
+for i, row in Main_Shocks.iterrows():
+    dwnstr=','+str(row['record_sequence_number'])+dwnstr
+    
+MSstr=dwnstr[1:]
+
+Main_Shocks.plot.scatter(x='joyner_booredist_km',y='earthquake_magnitude',s=20,c='earthquake_magnitude',colormap='viridis')
+plt.title('Main Shock Selection',fontsize=32)
+plt.xlabel('Rrup (km)',fontsize=24)
+plt.ylabel('Magnitude (Mw)',fontsize=24)
+plt.tick_params(direction='out',axis='both',labelsize=20)
+plt.show
+
+# Copying already Downloaded files in Kowalsky Group Folder:
+
+source = r"Q:\My Drive\NGA Ground Motion Downloads\01_AccelerationFiles"
+destination = r"C:\Users\vacalder\Documents\TimeDependent_PBEE\EarthquakeSelection\Mainshocks"
+
+for i, row in Main_Shocks.iterrows():
+    if row['downloaded']=="Yes":
+        for filename in os.listdir(r'Q:\My Drive\NGA Ground Motion Downloads\01_AccelerationFiles'):
+            if fnmatch.fnmatch(filename, 'RSN'+str(row['record_sequence_number'])+'*.AT2'):
+                print(filename)
+                filesrc=source+"\\"+filename
+                filedtn=destination+"\\"+filename
+                shutil.copyfile(filesrc,filedtn) 
+    else:
+        print('notdownloaded yet')
+
+
+# Accesing  PEER Website
+
+#browser = webdriver.Chrome() 
+#url="https://ngawest2.berkeley.edu/users/sign_in?unauthenticated=true"    
+#browser.get(url)
+#browser.find_element_by_id("user_email").send_keys('vacalder@ncsu.edu')
+#browser.find_element_by_id("user_password").send_keys("ViCk2016")
+#browser.find_element_by_id("user_submit").click()
+#url2="https://ngawest2.berkeley.edu/"
+#browser.get(url2)
+#browser.find_element_by_xpath('//*[@id="content"]/table[1]/tbody/tr[1]/td[2]/a').click()
+#browser.find_element_by_xpath('//*[@id="buttons"]/button').click()
+#WebDriverWait(browser, 10) # seconds
+#url4="https://ngawest2.berkeley.edu/spectras/223218/searches/new"
+#browser.get(url4)
+#browser.find_element_by_xpath('//*[@id="search_search_nga_number"]').send_keys(MSstr)
+#browser.find_element_by_xpath('//*[@id="new_search"]/div[2]/fieldset/button').click()
+#browser.find_element_by_xpath('//*[@id="middle_submit"]/fieldset[3]/button[2]').click()
+#browser.find_element_by_id('show-alert').click()
+#alert = browser.switch_to.alert
+#alert.accept()
+#alert.accept()
+    
+# Selecting Aftershocks and generate String to download RSN
+
+    
+for y in range (2,3):
+    print('the valu of y is ',y)
+    Aftershocks=Peer_Filtered[Peer_Filtered['earthquake_name'].str.contains('0'+str(y))]
+    
+    
+for y in range (3,8):
+    print('the valu of y is ',y)
+    df=Peer_Filtered[Peer_Filtered['earthquake_name'].str.contains('0'+str(y))]
+    Aftershocks=Aftershocks.append(df)
+
+for i, row in Aftershocks.iterrows():
+    dwnstr2=','+str(row['record_sequence_number'])+dwnstr
+
+ASstr=dwnstr2[1:]
+
+
+# Copying already Downloaded files in Kowalsky Group Folder:
+
+source = r"Q:\My Drive\NGA Ground Motion Downloads\01_AccelerationFiles"
+destination = r"C:\Users\vacalder\Documents\TimeDependent_PBEE\EarthquakeSelection\Aftershocks"
+
+for i, row in Aftershocks.iterrows():
+    if row['downloaded']=="Yes":
+        for filename in os.listdir(r'Q:\My Drive\NGA Ground Motion Downloads\01_AccelerationFiles'):
+            if fnmatch.fnmatch(filename, 'RSN'+str(row['record_sequence_number'])+'*.AT2'):
+                print(filename)
+                filesrc=source+"\\"+filename
+                filedtn=destination+"\\"+filename
+                shutil.copyfile(filesrc,filedtn) 
+    else:
+        print('notdownloaded yet')
+
+
+#browser = webdriver.Chrome() 
+#url="https://ngawest2.berkeley.edu/users/sign_in?unauthenticated=true"    
+#browser.get(url)
+#browser.find_element_by_id("user_email").send_keys('vacalder@ncsu.edu')
+#browser.find_element_by_id("user_password").send_keys("ViCk2016")
+#browser.find_element_by_id("user_submit").click()
+#url2="https://ngawest2.berkeley.edu/"
+#browser.get(url2)
+#browser.find_element_by_xpath('//*[@id="content"]/table[1]/tbody/tr[1]/td[2]/a').click()
+#browser.find_element_by_xpath('//*[@id="buttons"]/button').click()
+#WebDriverWait(browser, 10) # seconds
+#url4="https://ngawest2.berkeley.edu/spectras/223218/searches/new"
+#browser.get(url4)
+#browser.find_element_by_xpath('//*[@id="search_search_nga_number"]').send_keys(ASstr)
+#WebDriverWait(browser, 300) # seconds
+#browser.find_element_by_xpath('//*[@id="new_search"]/div[2]/fieldset/button').click()
+#WebDriverWait(browser, 300) # seconds
+#browser.find_element_by_xpath('//*[@id="middle_submit"]/fieldset[3]/button[2]').click()
+#browser.find_element_by_id('show-alert').click()
+#alert = browser.switch_to.alert
+#alert.accept()
+#alert.accept()
